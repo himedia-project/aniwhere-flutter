73435f4eb94e1c0a6ba1fd200dcd39276b6ca9a5
seonggon do <mooh2jj@naver.com>
2025-01-24 19:32:53 +0900

    장바구니 목록 주문하기 버튼 가운데 정렬

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -195,40 +195,30 @@ class _CartPageState extends State<CartPage> {
         },
       ),
       bottomNavigationBar: const CommonBottomNavigation(currentIndex: 2),
-      floatingActionButton: Align(
-        // alignment: Alignment.bottomCenter, // 하단 중앙에 배치
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0), // 좌우 간격
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Container(
-                height: 70, // 버튼의 높이
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue[100], // 배경색
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30), // 둥근 모서리
-                    ),
-                    textStyle: TextStyle(fontSize: 20), // 텍스트 크기
-                  ),
-                  onPressed: () async {
-                    // 주문하기 버튼 클릭 시 OrderPage로 이동
-                    Navigator.pushNamed(context, '/order', arguments: {
-                      'cartItems': futureCartItems,
-                    });
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.shopping_cart, size: 24), // 장바구니 아이콘 추가
-                      SizedBox(width: 10), // 아이콘과 텍스트 간격
-                      Text('장바구니 목록 주문하기'),
-                    ],
-                  ),
-                ),
-              ),]
+      floatingActionButton: Container(
+        width: MediaQuery.of(context).size.width * 0.9, // 화면 너비의 90%
+        padding: const EdgeInsets.only(bottom: 16.0), // 하단 패딩
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue[100],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 15.0), // 버튼 높이 조절
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+          onPressed: () async {
+            Navigator.pushNamed(context, '/order', arguments: {
+              'cartItems': futureCartItems,
+            });
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.shopping_cart, size: 24),
+              SizedBox(width: 10),
+              Text('장바구니 목록 주문하기'),
+            ],
           ),
         ),
       ),
